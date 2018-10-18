a573752b68c8c0b44cfe63ca47e1b0d939f664b6
samuel.dugger <samuel.dugger@iress.co.uk>
2018-10-18 08:20:41 +0100

    Completed

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -43,8 +43,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            BaseGradeBook gradeBook;
-            //gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;            
             if (type == "standard")
             {
                 gradeBook = new StandardGradeBook(name);
